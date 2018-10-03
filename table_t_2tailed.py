d0593186d13fbac55133ea9a514b46d63fdae601
Sébastien <31131320+tryptofan68@users.noreply.github.com>
2018-10-03 14:06:15 +0200

    Update table_t_2tailed.py

--- table_t_2tailed.py
+++ table_t_2tailed.py
@@ -24,7 +24,7 @@ with open('table_t_2-tailed.txt', 'w') as file:
 	
 	print(f"df\t{a:4.3f}\t{b:4.3f}\t{c:4.3f}\t{d:4.3f}\t{e:4.3f}\t{f:4.3f}", file=file)
 	print("-------------------------------------------------------", end='', file=file)
-	for df in range(1, 11):
+	for df in range(1, 16):
 		print(f"\n{df}", end="", file=file)
 		for alpha in [a,b,c,d,e,f]:
 			print(f"\t{stats.t(df=df).ppf((1+alpha)/2):4.3f}", end="", file=file)
